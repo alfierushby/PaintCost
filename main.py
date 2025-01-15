2d90c5ef16f289a3ed701f9e781ad934b5db7caf
ickeyben123 <rushbyalfie99@gmail.com>
2025-01-15 14:48:30 +0000

    Complete the cost calculation at the end

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def get_named_dimension(name):
+def get_named_dimension_square(name):
     wall_dimension_x = int(input(f"Enter the width of the {name} in meters: "))
     wall_dimension_y = int(input(f"Enter the height of the {name} in meters: "))
     return wall_dimension_x * wall_dimension_y
@@ -7,20 +7,24 @@ def get_named_dimension(name):
 if __name__ == '__main__':
     paint_cost = float(input("Enter the cost of your paint per square meter: "))
     paint_area = 0
-    obstruction_area = 0
 
     walls = True
 
     while walls:
-        paint_area += get_named_dimension("wall")
+        paint_area += get_named_dimension_square("wall")
         response = str(input("Do you want to add another wall? (Y or N)")).upper()
         if response == "N":
             walls = False
 
-    paint_area += get_named_dimension("Ceiling")
+    paint_area += get_named_dimension_square("Ceiling")
 
     response = str(input("Do you want to add any obstructions on your walls that you won't paint? (Y or N)")).upper()
     if response == "Y":
         obstructions = True
         while obstructions:
-            paint_area += get_named_dimension("obstruction")
+            paint_area -= get_named_dimension_square("obstruction")
+            response = str(input("Do you want to add another obstruction? (Y or N)")).upper()
+            if response == "N":
+                obstructions = False
+
+    print(f"The total cost of your paint is: {paint_area*paint_cost}")
