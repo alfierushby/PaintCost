12722527a442a7029e53b535d35fe80a00fdeddc
ickeyben123 <rushbyalfie99@gmail.com>
2025-01-15 14:41:05 +0000

    Add the ability to add walls to the rooms area

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-def get_wall_dimension():
-    wall_dimension_x = int(input("Enter the width of the wall in meters: "))
-    wall_dimension_y = int(input("Enter the height of the wall in meters: "))
-    return wall_dimension_x, wall_dimension_y
+def get_named_dimension(name):
+    wall_dimension_x = int(input(f"Enter the width of the {name} in meters: "))
+    wall_dimension_y = int(input(f"Enter the height of the {name} in meters: "))
+    return wall_dimension_x * wall_dimension_y
+
 
 if __name__ == '__main__':
     paint_cost = float(input("Enter the cost of your paint per square meter: "))
-    
+    paint_area = 0
+    obstruction_area = 0
 
+    walls = True
 
+    while walls:
+        paint_area += get_named_dimension("wall")
+        response = str(input("Do you want to add another wall? (Y or N)"))
+        if response == "N":
+            walls = False
