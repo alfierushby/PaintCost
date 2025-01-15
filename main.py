ab91d7107e65ea958c7ef918014f4d68949c2583
ickeyben123 <rushbyalfie99@gmail.com>
2025-01-15 14:55:55 +0000

    Add different shape functions and the ability to choose one

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
-def get_named_dimension_square(name):
-    wall_dimension_x = int(input(f"Enter the width of the {name} in meters: "))
-    wall_dimension_y = int(input(f"Enter the height of the {name} in meters: "))
-    return wall_dimension_x * wall_dimension_y
+import math
 
+
+def get_named_dimension_square(name):
+    dimension_base = int(input(f"Enter the width of the {name} in meters: "))
+    dimension_height = int(input(f"Enter the height of the {name} in meters: "))
+    return dimension_base * dimension_height
+
+def get_named_dimension_triangle(name):
+    dimension_base = int(input(f"Enter the base of the {name} in meters: "))
+    dimension_height = int(input(f"Enter the height of the {name} in meters: "))
+    return (dimension_base * dimension_height)/2
+
+def get_named_dimension_ellipse(name):
+    dimension_base = int(input(f"Enter the a-axis of the {name} in meters: "))
+    dimension_height = int(input(f"Enter the b-axis of the {name} in meters: "))
+    return math.pi * dimension_base * dimension_height
+
+def get_dimension(name):
+    dim_response = str(input(f"Enter the shape of the {name} with the option of 'square', 'triangle', 'ellipse': "))
+    match dim_response:
+        case 'square':
+            return get_named_dimension_square(name)
+        case 'triangle':
+            return get_named_dimension_triangle(name)
+        case 'ellipse':
+            return get_named_dimension_ellipse(name)
+        case _:
+            get_dimension(name)
 
 if __name__ == '__main__':
     paint_cost = float(input("Enter the cost of your paint per square meter: "))
@@ -11,21 +35,21 @@ if __name__ == '__main__':
     walls = True
 
     while walls:
-        paint_area += get_named_dimension_square("wall")
-        response = str(input("Do you want to add another wall? (Y or N)")).upper()
+        paint_area += get_dimension("wall")
+        response = str(input("Do you want to add another wall? (Y or N): ")).upper()
         if response == "N":
             walls = False
 
-    response = str(input("Do you want to add the ceiling? (Y or N)")).upper()
+    response = str(input("Do you want to add the ceiling? (Y or N): ")).upper()
     if response == "Y":
-        paint_area += get_named_dimension_square("Ceiling")
+        paint_area += get_dimension("Ceiling")
 
-    response = str(input("Do you want to add any obstructions on your walls that you won't paint? (Y or N)")).upper()
+    response = str(input("Do you want to add any obstructions on your walls that you won't paint? (Y or N): ")).upper()
     if response == "Y":
         obstructions = True
         while obstructions:
-            paint_area -= get_named_dimension_square("obstruction")
-            response = str(input("Do you want to add another obstruction? (Y or N)")).upper()
+            paint_area -= get_dimension("obstruction")
+            response = str(input("Do you want to add another obstruction? (Y or N): ")).upper()
             if response == "N":
                 obstructions = False
 
