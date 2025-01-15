43089412b645fa3ad9dfe840cdcd2421061a6f27
ickeyben123 <rushbyalfie99@gmail.com>
2025-01-15 14:36:09 +0000

    Add wall dimension function

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+def get_wall_dimension():
+    wall_dimension_x = int(input("Enter the width of the wall in meters: "))
+    wall_dimension_y = int(input("Enter the height of the wall in meters: "))
+    return wall_dimension_x, wall_dimension_y
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    paint_cost = float(input("Enter the cost of your paint per square meter: "))
+    
+
 
